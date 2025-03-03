36672379c3ea3d2d6276cbd9f5095b0406f2b287
Aditishukla5 <kumariaditi8220@gmail.com>
2025-03-03 10:49:28 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Pattern3
+{
+public static void main(String args[])
+{
+int i,j,row=5;
+for(i=1;i<=row;i++)
+{
+for(j=-1;j<=i;j++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
